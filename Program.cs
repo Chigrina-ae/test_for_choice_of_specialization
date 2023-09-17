5242c9d2f72b34411eb37b0de45b62934a5638af
Anna <aechigrina@gmail.com>
2023-09-18 00:12:15 +0300

    add program

--- Program.cs
+++ Program.cs
@@ -8,3 +8,39 @@
 [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 [“Russia”, “Denmark”, “Kazan”] → []
 */
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Количество элементов в массиве ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] arrayStrings = new string[size];
+for (int i = 0; i < size; i++)
+{
+    Console.WriteLine($"Введите {i + 1} элемент: ");
+    string symbol = Console.ReadLine()!;
+    arrayStrings[i] = symbol;
+}
+
+string[] arrayFinal = new string[size];
+int length = 3;
+int count = 0;
+
+for (int i = 0; i < size; i++)
+{
+    if (arrayStrings[i].Length <= length)
+    {
+        arrayFinal[count] = arrayStrings[i];
+        count++;
+    }
+}
+
+Console.WriteLine();
+PrintArray(arrayFinal);
